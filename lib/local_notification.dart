1b1d4c2e3d0becc6d8c2e71a547236f5f82d6345
frendy app <frendyapp@gmail.com>
2020-09-15 10:02:42 -0700

    first commit

---
+++ lib/local_notification.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotification {
+  static FlutterLocalNotificationsPlugin flutterNotificationPlugin;
+  static AndroidNotificationDetails androidSettings;
+
+  static Initializer() {
+    flutterNotificationPlugin = FlutterLocalNotificationsPlugin();
+    androidSettings = AndroidNotificationDetails(
+        "111", "Background_task_Channel", "Channel to test background task",
+        importance: Importance.High, priority: Priority.Max);
+    var androidInitialization = AndroidInitializationSettings('app_icon');
+    var initializationSettings =
+        InitializationSettings(androidInitialization, null);
+    flutterNotificationPlugin.initialize(initializationSettings,
+        onSelectNotification: onNotificationSelect);
+  }
+
+  static Future<void> onNotificationSelect(String payload) async {
+    print(payload);
+  }
+
+  static ShowOneTimeNotification(DateTime scheduledDate) async {
+    var notificationDetails = NotificationDetails(androidSettings, null);
+    await flutterNotificationPlugin.schedule(
+        1,
+        "Background Task notification",
+        "This is a background task notification",
+        scheduledDate,
+        notificationDetails,
+        androidAllowWhileIdle: true);
+  }
+}
